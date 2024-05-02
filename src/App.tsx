05d0d0bdb20dc09ce4b6159a71b75d77580b3143
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-02 17:06:03 -0400

    block10

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Block1 } from "./blocks/block1/block1";
 import { Block2 } from "./blocks/block2/block2";
 import { Block4 } from "./blocks/block4/block4";
 import { Block3 } from "./blocks/block3/block3";
+import { Block10 } from "./blocks/block10/block10";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Block2 />
       <Block3 />
       <Block4 />
+      <Block10 />
     </div>
   );
 }
---
+++ src/blocks/block10/block10.tsx
@@ -0,0 +1,7 @@
+export const Block10 = () => {
+  return (
+    <div className="min-h-screen w-full flex justify-center align-center">
+      <img src="./ass.png" />
+    </div>
+  );
+};
