95e61257ef4d0e43424776a4d4996c0a4a98ee1b
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-05 16:58:15 -0400

    links done

--- src/urls.ts
+++ src/urls.ts
@@ -1,15 +1,12 @@
 export const CA = "Coming...";
-export const TELEGRAM = "";
-export const TWITTER = "";
-export const BUY_LINK = "";
-export const DEXSCREENER =
-  "https://dexscreener.com/solana/b4hnbpkthpodjfkxwg1cqhhdygei4uhcebt61j4uk5tor";
-export const RAYDIUM =
-  "https://raydium.io/swap/?outputCurrency=3uPF7WqZDbhYQNmJYfmc66333Qmsc9ohYjJ8p9B5qKrgNp";
-export const JUPITER =
-  "https://jup.ag/swap/So11111111111111111111111111111111111111112-3uP234F7WqZDbhYQNmJYfmc663Qmsc9ohYjJ8p9B5qKrgNp?inAmount=";
-export const DEXTOOLS =
-  "https://www.dextools.io/app/en/solana/pair-explorer/B4HNBPkTHpodjfkxWg1CQhH34dYgei4uhcEBt6j4uK5Tor?t=1714707732011";
+export const TELEGRAM = "https://t.me/peritosolana";
+export const TWITTER = "https://x.com/peritosolana";
 
-export const TELEGRAM_DEV = "";
-export const TWITTER_DEV = "";
+export const BUY_LINK = "";
+export const DEXSCREENER = "";
+export const RAYDIUM = "";
+export const JUPITER = "";
+export const DEXTOOLS = "";
+
+export const TELEGRAM_DEV = "https://t.me/lettodev";
+export const TWITTER_DEV = "https://twitter.com/letto_dev";
