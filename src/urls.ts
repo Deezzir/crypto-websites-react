1e18bd3e44519b55866e0f9e57826822746342d3
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-05 19:56:31 -0400

    links updated

--- src/urls.ts
+++ src/urls.ts
@@ -2,10 +2,13 @@ export const CA = "ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq";
 export const TELEGRAM = "https://t.me/peritosolana";
 export const TWITTER = "https://x.com/peritosolana";
 
-export const BUY_LINK = "";
+export const BUY_LINK =
+  "https://jupiter-terminal.dexscreener.com/?inputMint=ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq&outputMint=So11111111111111111111111111111111111111112";
 export const DEXSCREENER = "";
-export const RAYDIUM = "";
-export const JUPITER = "";
+export const RAYDIUM =
+  "https://raydium.io/swap/?outputCurrency=7Cga2epeJMhte3j74ekEBRMBDn2nRKcgFBrEggSC99fk";
+export const JUPITER =
+  "https://jupiter-terminal.dexscreener.com/?inputMint=ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq&outputMint=So11111111111111111111111111111111111111112";
 export const DEXTOOLS = "";
 
 export const TELEGRAM_DEV = "https://t.me/lettodev";
