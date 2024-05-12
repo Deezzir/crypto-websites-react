6bba72504bfe1042706abf61f0bd90fdb8c26292
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-12 19:33:55 -0400

    cleanup

--- src/common/footer.tsx
+++ src/common/footer.tsx
@@ -1,9 +1,3 @@
-import { TgIcon } from "./icons/tg-icon";
-import { XIcon } from "./icons/x-icon";
-import { TELEGRAM_DEV, TWITTER_DEV } from "./urls";
-
-const iconClass = "w-6 h-6 fill-white";
-
 export const FooterSection = () => {
   return (
     <footer className="px-8 pt-1 text-center bg-[#1f2937] text-white backdrop-blur transition-colors duration-500">
@@ -11,24 +5,6 @@ export const FooterSection = () => {
         <span className="text-sm font-bold sm:text-xl ">
           $TRUMPY &copy; 2024
         </span>
-        {/* <span className="inline-flex">
-          <a
-            href={TWITTER_DEV}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-[2px] rounded p-2 font-bold"
-          >
-            <XIcon iconClass={iconClass} />
-          </a>
-          <a
-            href={TELEGRAM_DEV}
-            rel="noopener noreferrer"
-            target="_blank"
-            className="mx-[2px] rounded p-2 font-bold"
-          >
-            <TgIcon iconClass={iconClass} />
-          </a>
-        </span> */}
       </p>
       <p className="px-2 pb-2 text-sm">
         $TRUMPY is a memecoin with no intrinsic value, and is not a investment
--- src/common/urls.ts
+++ src/common/urls.ts
@@ -1,19 +1,12 @@
-export const CA = "ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq";
-export const TELEGRAM = "https://t.me/peritosolana";
-export const TWITTER = "https://x.com/peritosolana";
+export const CA = "wefEWF@f24f2f2f2f";
+export const TELEGRAM = "";
+export const TWITTER = "";
 
-export const BUY_LINK =
-  "https://jup.ag/swap/ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq-SOL";
-export const DEXSCREENER =
-  "https://dexscreener.com/solana/71yaKMBhXHjh3dutwd221fkTnfxZULEcYeDzpzB5oUm4";
-export const RAYDIUM =
-  "https://raydium.io/swap/?outputCurrency=ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq";
-export const JUPITER =
-  "https://jup.ag/swap/ucS24PgtPbjWfSgtMB2ttCqXRYayA2ZsJKGz28nksQq-SOL";
+export const BUY_LINK = "";
+export const DEXSCREENER = "";
+export const RAYDIUM = "";
+export const JUPITER = "";
 export const DEXTOOLS = "";
-
-export const TELEGRAM_DEV = "https://t.me/lettodev";
-export const TWITTER_DEV = "https://twitter.com/letto_dev";
 
 export const RPC_ENDPOINT =
   "https://withered-crimson-sun.solana-devnet.quiknode.pro/5df09d3d4a53046d05db415a893610388d405d59/";
